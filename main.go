65e140555e1dd9d68bcb7556475e8435edebfedc
oomale <oamlegrace2009@gmail.com>
2026-08-12 15:15:10 +0100

    feat: set up Go server, templates, styling, and auth pages

--- main.go
+++ main.go
@@ -16,9 +16,36 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/login.html"))
+
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+}
+
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/signup.html"))
+
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/signup", signupHandler)
+
 	fmt.Println("Server Running on https://localhost:8080")
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err)
