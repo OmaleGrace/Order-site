67ab0ec11ac427c31bf190a8b6cd6f846d57f9c9
Omale Grace <omalegrace2009@gmail.com>
2026-08-27 13:54:36 +0000

    adding checkout

--- main.go
+++ main.go
@@ -286,6 +286,77 @@ func removeFromCartHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	http.Redirect(w, r, "/cart", http.StatusSeeOther)
 }
 
+func checkoutHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	cookie, err := r.Cookie("user_id")
+	if err != nil {
+		http.Error(w, "You must be logged in", http.StatusUnauthorized)
+		return
+	}
+
+	userID, err := strconv.Atoi(cookie.Value)
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	items, err := cart.GetItems(db, userID)
+	if err != nil {
+		http.Error(w, "Could not load cart", http.StatusInternalServerError)
+		return
+	}
+
+	if len(items) == 0 {
+		http.Error(w, "Your cart is empty", http.StatusBadRequest)
+		return
+	}
+
+	var totalKobo int
+
+	for _, item := range items {
+		totalKobo += item.PriceKobo * item.Quantity
+	}
+
+	var orderID int
+
+	err = db.QueryRow(`
+		INSERT INTO orders (user_id, total_kobo, status)
+		VALUES ($1, $2, 'pending')
+		RETURNING id
+	`, userID, totalKobo).Scan(&orderID)
+
+	if err != nil {
+		fmt.Println("Create order error:", err)
+		http.Error(w, "Could not create order", http.StatusInternalServerError)
+		return
+	}
+
+	for _, item := range items {
+		_, err = db.Exec(`
+			INSERT INTO order_items
+			(order_id, menu_item_id, quantity, price_kobo)
+			VALUES ($1, $2, $3, $4)
+		`,
+			orderID,
+			item.MenuItemID,
+			item.Quantity,
+			item.PriceKobo,
+		)
+
+		if err != nil {
+			fmt.Println("Create order item error:", err)
+			http.Error(w, "Could not create order items", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "Order #%d created successfully. Payment integration coming next.", orderID)
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -321,6 +392,9 @@ func main() {
 	http.HandleFunc("/cart/remove", func(w http.ResponseWriter, r *http.Request) {
 		removeFromCartHandler(w, r, db)
 	})
+	http.HandleFunc("/checkout", func(w http.ResponseWriter, r *http.Request) {
+	checkoutHandler(w, r, db)
+})
 
 	fmt.Println("Server Running on https://localhost:8080")
 
